a570183004de0e2e80848b1fe15f52cd64161606
Ben Mewburn <ben@desktop.mewburn>
2017-04-12 23:51:15 +1000

    license, package.json, config

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,7 +1,6 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
+/* Copyright (c) Ben Robert Mewburn 
+ * Licensed under the ISC Licence.
+ */
 'use strict';
 
 import * as path from 'path';
