919339782f3db4d77d0182d3534b0554d3a5b272
Ben Mewburn <ben@mewburn.id.au>
2019-06-25 09:17:01 +1000

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+/**@type {import('webpack').Configuration}*/
+const config = {
+  target: 'node',
+
+  entry: './src/extension.ts',
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2',
+    devtoolModuleFilenameTemplate: '../[resource-path]'
+  },
+  devtool: 'source-map',
+  externals: {
+    vscode: 'commonjs vscode'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  "mode": "production"
+};
+module.exports = config;
